3189e9b0cc1e2f313380165e73806c07bdc7aea5
Daniil <steam.haver@gmail.com>
2018-05-01 17:56:08 +0400

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -5,6 +5,12 @@ struct trie{
     trie(){}
     trie* add(char c){
         if(!nxt.count(c))nxt[c]=new trie();
+        nxt[c]->sz++;
         return nxt[c];
     }
+	void add(str s){
+		trie*cur=this;
+		for(char i:s)cur=add(i);
+		cur->en=1;
+	}
 };
