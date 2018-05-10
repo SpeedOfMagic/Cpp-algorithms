a8968555d3ece7d5c7ac5674f8d0fb496f16412b
Daniil <steam.haver@gmail.com>
2018-05-10 15:25:41 +0400

    Create Z-algorithm.cpp

---
+++ Z-algorithm.cpp
@@ -0,0 +1,15 @@
+vector<int> algorithmZ(string S){
+    int l=0, r=0;
+    vector<int> Z;
+    Z.push_back(S.length());
+    for (int i=1;i<S.length();i++){
+        if (i>r || Z[i-l]>=r-i+1){ 
+            r=max(r,i);
+            l=i;
+            while (r<S.length() && S[r-l]==S[r]) r++;
+            Z.push_back(r-i);
+            r--;
+        } else Z.push_back(Z[i-l]);
+    }
+    return Z;
+}
