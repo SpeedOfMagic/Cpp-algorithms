7ddda476c2c9cbf973198980286d1c8e20007cc4
Daniil <steam.haver@gmail.com>
2018-05-12 20:44:31 +0400

    Create Conversions.cpp

---
+++ Conversions.cpp
@@ -0,0 +1,11 @@
+string inttostr(int a){
+    string res="";
+    for(;a;a/=10)res+=a%10+'0';
+    reverse(res.begin(),res.end());
+    return res;
+}
+int strtoint(string a){
+    int res=0;
+    for(char i:a)res=res*10+(i-'0');
+    return res;
+}
