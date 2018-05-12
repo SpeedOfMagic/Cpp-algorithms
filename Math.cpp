267f0d49f47a4390435a2e7d6d404831a931a860
Daniil <steam.haver@gmail.com>
2018-05-12 16:47:57 +0400

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -6,8 +6,8 @@ int gcd(int a,int b){
     return b;
 }
 int lcm(int a,int b){return a/gcd(a,b)*b;}
-int fact(int a,int mod){if (a==1)return 1;else return (a*fact(a-1,mod))%mod;}
-int sign(int a){return max(min((int)1,a),(int)-1);}
+int fact(int a,int mod){if (a==0)return 1%mod;else return (a*fact(a-1,mod))%mod;}
+int sign(int a){if(a>0)return 1;if(a<0)return -1;return 0;}
 bool prime(int a){
     if(a<2)return 0;if(a==2)return 1;
     for(int b=2;b<=ceil(sqrt(a));b++)if(a%b==0)return 0;
