0e997043963573a4fb341206cd6484a94eb79751
Daniil <steam.haver@gmail.com>
2018-05-16 19:17:37 +0400

    Update Conversions.cpp

--- Conversions.cpp
+++ Conversions.cpp
@@ -10,3 +10,14 @@ int strtoint(string a){
     for(char i:a)res=res*10+(i-'0');
     return res;
 }
+double strtod(string a){
+    double res=0;
+    double afterDot=1;
+    bool dotBegins=0;
+    for(char i:a){
+        if(dotBegins)afterDot*=10;
+        if(i=='.')dotBegins=1;
+        else res=(res*10)+(i-'0');
+    }
+    return res/afterDot;
+}
