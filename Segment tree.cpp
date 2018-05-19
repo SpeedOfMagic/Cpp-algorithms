e11f5d834bc4ef1bc6b94801623588c62bd8a71e
Daniil <steam.haver@gmail.com>
2018-05-19 13:14:00 +0400

    Update Segment tree.cpp

--- Segment tree.cpp
+++ Segment tree.cpp
@@ -5,18 +5,25 @@ const int nothing=0; //nothing means that f(x, nothing)=x
 struct segTreeNode{
     int val=nothing;
     segTreeNode(){}
+    segTreeNode *left=nullptr,*right=nullptr;
     void update(int val){ //function that updates node based on val
-        this->val=f(this->val,val);
+        this->val+=val;
+    }
+    void update(){ //function that updates node based on children
+        this->val=f(this->left->val,this->right->val);
     }
 };
 v<segTreeNode> segTree;
 int n,tot;
 void update(int nodeToUpdate,int s,int cur=1,int ll=1,int rr=tot){
-    segTree[cur].update(s);
-    if(ll==nodeToUpdate&&nodeToUpdate==rr)return;
+    if(ll==nodeToUpdate&&nodeToUpdate==rr){
+        segTree[cur].update(s);
+        return;
+    }
     int p=(ll+rr)/2;
     if(nodeToUpdate<=p)update(nodeToUpdate,s,cur*2,ll,p);
     else update(nodeToUpdate,s,cur*2+1,p+1,rr);
+    segTree[cur].update();
 }
 int query(int l,int r,int cur=1,int ll=1,int rr=tot){
     if(l>r)return nothing;
@@ -30,5 +37,9 @@ void init(vector<int> vals){
     while(tot&(tot-1))tot++;
     segTree=vector<segTreeNode>(2*tot);
     rep(i,tot,tot+n)segTree[i].val=vals[i-tot];
-    for(int i=tot-1;i;i--)segTree[i].val=f(segTree[i*2].val,segTree[i*2+1].val);
+    for(int i=tot-1;i;i--){
+        segTree[i].left=&segTree[i*2];
+        segTree[i].right=&segTree[i*2+1];
+        segTree[i].update();
+    }
 }
