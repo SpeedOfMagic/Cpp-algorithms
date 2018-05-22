68131c09feb18483378e1bd2be5025c752225c7c
Daniil <steam.haver@gmail.com>
2018-05-22 17:24:55 +0400

    Update Hashing.cpp

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,12 +1,15 @@
 const long long mod2 = 36028797018963913; //2^55-55
 const long long base = 26; //alphabet size
 const char startingLetter = 'a'; //starting letter of the alphabet
+
 long long hashChar(char c){
     return c - startingLetter + 1;   
 }
+
 long long hashStr(long long prevHash, char curChar){
     return ((prevHash * 26) % mod2 + hashChar(curChar)) % mod2;
 }
+
 long long hashStr(string& s){
     long long res = 0;
     for(char i : s)
