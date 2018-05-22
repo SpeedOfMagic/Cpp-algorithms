2232da20490de22fc8d88fe148612a6547417b7c
Daniil <steam.haver@gmail.com>
2018-05-22 17:35:27 +0400

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -1,12 +1,16 @@
 int gcd(int a, int b) {
     a = abs(a);
     b = abs(b);
+    
     if (a == 0 && b == 0)
         return 0;
+    
     if (a > b)
         swap(a, b);
+    
     if (a > 0)
         return gcd(a, b % a);
+    
     return b;
 }
 
@@ -24,29 +28,53 @@ int fact(int a, int mod) {
 int sgn(int a) {
     if (a > 0)
         return 1;
+    
     if (a < 0)
         return -1;
+    
     return 0;
 }
 
 bool prime(int a) {
     if (a < 2)
         return 0;
+    
     if (a == 2)
         return 1;
+    
     for (int b = 2; b <= ceil(sqrt(a)); b++)
         if (a % b == 0)
             return 0;
+    
     return 1;
 }
 int pow(int a, int b, int mod) {
     if (b == 0)
         return 1 % mod;
+    
     if (b == 1)
         return a % mod;
+    
     int res = pow(a, b/2, mod);
     res = (res * res) % mod;
+    
     if (b % 2 == 1)
         return (a * res) % mod;
+    
     return res;
+}
+vector<int> divisors(int a) {
+    vector<int> result;
+    
+    for (int i = 1; i <= ceil(sqrt(a)); i++){
+        if (a % i == 0){
+            result.push_back(i);
+            
+            if (a / i != i)
+                result.push_back(a / i);
+        }
+    }
+    
+    sort(result.begin(), result.end());
+    return result;
 }
