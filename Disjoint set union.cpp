3a467f4af8805f12f6c91cadb66cbc31f7bb2224
Daniil <steam.haver@gmail.com>
2018-05-22 17:24:09 +0400

    Update Disjoint set union.cpp

--- Disjoint set union.cpp
+++ Disjoint set union.cpp
@@ -1,18 +1,28 @@
-const int N=3e5+1;
-vector<int> repr(N),siz(N, 1);
+const int N = 1e6 + 1;
+vector<int> repr(N);
+vector<int> siz(N, 1);
+
 int find(int a){
-    if(repr[a]==a)return a;
-    else return repr[a]=find(repr[a]);
+    if(repr[a] == a)
+        return a;
+    else
+        return repr[a] = find(repr[a]);
 }
+
 void unite(int a,int b){
-    if(siz[a]>siz[b])swap(a,b);
-    a=find(a);
-    b=find(b);
-    repr[a]=b;
-    siz[b]+=siz[a];
+    if(siz[a] > siz[b])
+        swap(a,b);
+    a = find(a);
+    b = find(b);
+    repr[a] = b;
+    siz[b] += siz[a];
 }
-bool connected(int a,int b){return find(a)==find(b);}
+
+bool connected(int a,int b){
+    return find(a) == find(b);
+}
+
 void init(){
-    for(int i=0;i<N;i++)
-        repr[i]=i;
+    for(int i = 0;i < N;i++)
+        repr[i] = i;
 }
