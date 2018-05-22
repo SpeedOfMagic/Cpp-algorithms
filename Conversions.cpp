0b830c32b47f8ee69ef5319e3a012209e60ca1f8
Daniil <steam.haver@gmail.com>
2018-05-22 16:30:53 +0400

    Update Conversions.cpp

--- Conversions.cpp
+++ Conversions.cpp
@@ -1,23 +1,28 @@
 string inttostr(int a){
-    if(a==0)return "0";
-    string res="";
-    for(;a;a/=10)res+=a%10+'0';
-    reverse(res.begin(),res.end());
+    if(a == 0)return "0";
+    string res = "";
+    for(;a > 0; a/=10)
+        res += a % 10 + '0';
+    reverse(res.begin(), res.end());
     return res;
 }
 int strtoint(string a){
     int res=0;
-    for(char i:a)res=res*10+(i-'0');
+    for(char i : a)
+        res = res * 10 + (i - '0');
     return res;
 }
 double strtod(string a){
-    double res=0;
-    double afterDot=1;
-    bool dotBegins=0;
-    for(char i:a){
-        if(dotBegins)afterDot*=10;
-        if(i=='.')dotBegins=1;
-        else res=(res*10)+(i-'0');
+    double res = 0;
+    double afterDot = 1;
+    bool dotBegins = 0;
+    for(char i : a){
+        if(dotBegins)
+            afterDot *= 10;
+        if(i == '.')
+            dotBegins = 1;
+        else
+            res = (res * 10) + (i - '0');
     }
-    return res/afterDot;
+    return res / afterDot;
 }
