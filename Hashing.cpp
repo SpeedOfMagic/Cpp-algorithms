98e97b98aee2982fe3a6925491da717ec1658819
Daniil <steam.haver@gmail.com>
2018-05-22 16:33:21 +0400

    Update Hashing.cpp

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,8 +1,11 @@
-const int mod2=36028797018963913; //2^55-55
-int hashStr(str& s){
-    int base=26;
-    int res=0;
-    for(int i=0;i<sz(s);i++)res=((res*base)%mod2+s[i]-'a'+1)%mod2;
-    return (res+mod2)%mod2;
+const long long mod2 = 36028797018963913; //2^55-55
+long long hashStr(string& s){
+    long long base = 26;
+    long long res = 0;
+    for(int i = 0; i < (int) s.length(); i++)
+        res=((res * base) % mod2 + (s[i] - 'a' + 1)) % mod2;
+    return (res + mod2) % mod2;
 }
-int hashStr(int prevHash,char curChar){return ((prevHash*26)%mod2+curChar-'a'+1)%mod2;}
+int hashStr(int prevHash, char curChar){
+    return ((prevHash * 26) % mod2 + (curChar - 'a' + 1)) % mod2;
+}
