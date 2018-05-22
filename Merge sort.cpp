ee479984d95322bc6ce6491a76718f2aa5e7fdd3
Daniil <steam.haver@gmail.com>
2018-05-22 17:37:42 +0400

    Update Merge sort.cpp

--- Merge sort.cpp
+++ Merge sort.cpp
@@ -1,6 +1,7 @@
 const int N = 1e6 + 1;
 int a[N];
 int b[N];
+
 void mergesort(int l, int r) {
     if (r - l + 1 < 2)
         return;
@@ -13,5 +14,5 @@ void mergesort(int l, int r) {
     merge(a + l, a + cur + 1, a + cur + 1, a + r + 1, b);
     
     for(int i = l;i <= r; i++)
-        a[i]=b[i-l];
+        a[i] = b[i-l];
 }
