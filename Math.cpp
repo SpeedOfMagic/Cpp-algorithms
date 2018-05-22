43722278abcf0bd3d779be4a8910b1bce7ad9a57
Daniil <steam.haver@gmail.com>
2018-05-22 17:31:34 +0400

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -1,23 +1,52 @@
-int gcd(int a,int b){
-    a=abs(a); b=abs(b);
-    if (a==0 && b==0) return 0;
-    if (a>b)swap(a,b);
-    if (a)return gcd(a,b%a);
+int gcd(int a, int b) {
+    a = abs(a);
+    b = abs(b);
+    if (a == 0 && b == 0)
+        return 0;
+    if (a > b)
+        swap(a, b);
+    if (a > 0)
+        return gcd(a, b % a);
     return b;
 }
-int lcm(int a,int b){return a/gcd(a,b)*b;}
-int fact(int a,int mod){if (a==0)return 1%mod;else return (a*fact(a-1,mod))%mod;}
-int sign(int a){if(a>0)return 1;if(a<0)return -1;return 0;}
-bool prime(int a){
-    if(a<2)return 0;if(a==2)return 1;
-    for(int b=2;b<=ceil(sqrt(a));b++)if(a%b==0)return 0;
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+int fact(int a, int mod) {
+    if (a == 0)
+        return 1 % mod;
+    else
+        return (a * fact(a - 1, mod)) % mod;
+}
+
+int sgn(int a) {
+    if (a > 0)
+        return 1;
+    if (a < 0)
+        return -1;
+    return 0;
+}
+
+bool prime(int a) {
+    if (a < 2)
+        return 0;
+    if (a == 2)
+        return 1;
+    for (int b = 2; b <= ceil(sqrt(a)); b++)
+        if (a % b == 0)
+            return 0;
     return 1;
 }
-int pow(int a,int b,int mod){
-    if(b==0)return 1;
-    if(b==1)return a%mod;
-    int res=pow(a,b/2,mod);
-    res=(res*res)%mod;
-    if(b%2)return (a*res)%mod;
+int pow(int a, int b, int mod) {
+    if (b == 0)
+        return 1 % mod;
+    if (b == 1)
+        return a % mod;
+    int res = pow(a, b/2, mod);
+    res = (res * res) % mod;
+    if (b % 2 == 1)
+        return (a * res) % mod;
     return res;
 }
