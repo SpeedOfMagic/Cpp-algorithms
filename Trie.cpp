832fcb4d65cb11180a778f3c280dcb54b10a69d6
Daniil <steam.haver@gmail.com>
2018-05-23 21:39:26 +0400

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,23 +1,28 @@
 struct trie{
     int size = 0;
     bool end = 0;
-    map<char, trie*>nxt;
-    trie(){}
+    map<char, trie*> nxt;
+	
+    trie() {}
 };
 
-int size(trie* cur){
+int size(trie* cur) {
     if (cur == nullptr)
         return 0;
     else
         return cur -> size;
 }
-void add(str s,trie* root,int num){
-    trie* cur=root;
-	cur->size++;
-    for(char i:s){
-        if(cur->nxt[i]==nullptr)cur->nxt[i]=new trie();
-        cur=cur->nxt[i];
-		cur->size++;
+
+void add(str s, trie* root){
+    trie* cur =r oot;
+	cur -> size++;
+	
+    for (char i : s) {
+        if(cur -> nxt[i] == nullptr)
+			cur -> nxt[i] = new trie();
+        cur = cur -> nxt[i];
+		cur -> size++;
 	}
-    cur->end=1;
+	
+    cur -> end = 1;
 }
