863050a91f04b631eac661b67f3b59bacbfc021f
Daniil <steam.haver@gmail.com>
2018-06-17 23:53:21 +0400

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -1,13 +1,13 @@
 int gcd(int a, int b) {
     a = abs(a);
     b = abs(b);
-    
+
     if (a > b)
         swap(a, b);
-    
+
     if (a > 0)
         return gcd(a, b % a);
-    
+
     return b;
 }
 
@@ -19,39 +19,39 @@ int lcm(int a, int b) {
 int sgn(int a) {
     if (a > 0)
         return 1;
-    
+
     if (a < 0)
         return -1;
-    
+
     return 0;
 }
 
 bool prime(int a) {
     if (a < 2)
         return 0;
-    
+
     if (a == 2)
         return 1;
-    
+
     for (int b = 2; b <= ceil(sqrt(a)); b++)
         if (a % b == 0)
             return 0;
-    
+
     return 1;
 }
 
 vector<int> divisors(int a) {
     vector<int> result;
-    
+
     for (int i = 1; i <= ceil(sqrt(a)); i++){
         if (a % i == 0){
             result.push_back(i);
-            
+
             if (a / i != i)
                 result.push_back(a / i);
         }
     }
-    
+
     sort(result.begin(), result.end());
     return result;
 }
@@ -60,40 +60,44 @@ vector<int> divisors(int a) {
 int euler(int a) {
     if (a == 1)
         return 0;
-    
+
     return (int) divisors(a).size() - 2;
 }
 
-int pow(int a, int b, int mod) {
+const int mod = 1e9 + 7;
+
+int pow(int a, int b) {
     if (b == 0)
         return 1 % mod;
-    
+
     if (b == 1)
         return a % mod;
-    
-    int res = pow(a, b/2, mod);
+
+    int res = pow(a, b/2);
     res = (res * res) % mod;
-    
+
     if (b % 2 == 1)
         return (a * res) % mod;
-    
+
     return res;
 }
 
 //uses pow
-int inv(int a, int mod) {
+int inv(int a) {
     int b = mod - 2;
-    return pow(a, b, mod);
+    return pow(a, b);
 }
 
-int fact(int a, int mod) {
-    if (a == 0)
-        return 1 % mod;
-    else
-        return (a * fact(a - 1, mod)) % mod;
+vector<int> factorials = {1};
+void initFactorials(unsigned int n) { //calculates [1! % mod, 2! % mod, ... , n! % mod]
+    for (unsigned int i = factorials.size(); i <= n; i++) {
+        int d = factorials.back();
+        factorials.push_back((d * i) % mod);
+    }
 }
 
-//uses fact, inv; you want to make array of factorials if you will use this function often
-int c(int k, int n, int mod){
-    return ((fact(n, mod) * inv(fact(k, mod), mod)) % mod * inv(fact(n - k, mod), mod)) % mod;
+//uses initFactorials, inv;
+int c(int k, int n){
+    initFactorials(n);
+    return ((factorials[n] * inv(factorials[k])) % mod * inv(factorials[n - k])) % mod;
 }
