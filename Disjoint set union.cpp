cb518ff8a81ecabd14f17a878e51718971afcfa7
Daniil <steam.haver@gmail.com>
2018-07-03 14:49:50 +0400

    Update Disjoint set union.cpp

--- Disjoint set union.cpp
+++ Disjoint set union.cpp
@@ -1,33 +1,35 @@
-const int N = 1e6 + 1;
-int repr[N];
-int siz[N];
 
-int find(int a) {
-    if (repr[a] == a)
-        return a;
-    else
-        return repr[a] = find(repr[a]);
-}
+struct dsu {
+    vector<int> repr;
+    vector<int> siz;
 
-void unite(int a,int b) {
-    a = find(a);
-    b = find(b);
-    
-    if (siz[a] > siz[b])
-        swap(a,b);
-    
-    repr[a] = b;
-    
-    siz[b] += siz[a];
-}
-
-bool connected(int a,int b) {
-    return find(a) == find(b);
-}
-
-void init() {
-    for(int i = 0; i < N; i++){
-        repr[i] = i;
-        siz[i] = 1;
+    int fin(int a) {
+        if (repr[a] == a)
+            return a;
+        else
+            return repr[a] = fin(repr[a]);
     }
-}
+
+    void unite(int a, int b) {
+        a = fin(a);
+        b = fin(b);
+
+        if (siz[a] > siz[b])
+            swap(a, b);
+
+        repr[a] = b;
+
+        siz[b] += siz[a];
+    }
+
+    bool connected(int a, int b) {
+        return fin(a) == fin(b);
+    }
+
+    dsu(int n) {
+        for(int i = 0; i < n; i++) {
+            repr.push_back(i);
+            siz.push_back(1);
+        }
+    }
+};
