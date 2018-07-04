3213bb47639d4e438f8f024bc4aa468671d01968
Daniil <steam.haver@gmail.com>
2018-07-04 11:13:00 +0400

    Update Z-algorithm.cpp

--- Z-algorithm.cpp
+++ Z-algorithm.cpp
@@ -1,24 +1,23 @@
 vector<int> algorithmZ(string S) {
-    int l = 0;
-    int r = 0;
+    unsigned int l = 0;
+    unsigned int r = 0;
     vector<int> Z;
     
     Z.push_back(S.length());
     
-    for (int i = 1; i < S.length(); i++){
-        if (i > r || Z[i-l] >= r - i + 1){ 
-            r = max(r,i);
+    for (unsigned int i = 1; i < S.length(); i++)
+        if (i > r || Z[i - l] >= r - i + 1) { 
             l = i;
+            r = max(r, i);
             
-            while (r < S.length() && S[r-l] == S[r])
+            while (r < S.length() && S[r - l] == S[r])
                 r++;
             
-            Z.push_back(r-i);
+            Z.push_back(r - i);
             
             r--;
         } else
-            Z.push_back(Z[i-l]);
-    }
+            Z.push_back(Z[i - l]);
     
     return Z;
 }
