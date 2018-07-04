7f71d1d4d4db4b9c4c7b1c0fec41a8e21cd614a1
Daniil <steam.haver@gmail.com>
2018-07-04 15:43:42 +0400

    Update Array inversions.cpp

--- Array inversions.cpp
+++ Array inversions.cpp
@@ -1,17 +1,17 @@
 vector<int> a;
-int calculateInversions(int l = 0, int r = a.size() - 1) {
+int calculateInversions(int l, int r) {
     if (l == r)
-        return;
+        return 0;
     else if (l + 1 == r) {
         if (a[l] > a[r]) {
-            ans++;
             swap(a[l], a[r]);
+            return 1;
         }
-        return;
+        return 0;
     }
 
     int middle = (l + r) / 2;
-    
+
     int res = 0;
 
     res += calculateInversions(l, middle);
@@ -31,6 +31,6 @@ int calculateInversions(int l = 0, int r = a.size() - 1) {
     }
     rep(i, l, r + 1)
         a[i] = b[i - l];
-    
-    return left;
+
+    return res;
 }
