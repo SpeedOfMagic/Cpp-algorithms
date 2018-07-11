c26ab0fec2bb601b6d9acc38ad7f7b3f8e6cb4da
Daniil <steam.haver@gmail.com>
2018-07-11 18:45:22 +0400

    Update Bits.cpp

--- Bits.cpp
+++ Bits.cpp
@@ -1,6 +1,16 @@
-bool isPowerOfTwo(int a){
+bool isPowerOfTwo(int a) {
     return ! (a & (a - 1));
 }
+
 int lsb(int a){
     return a & (-a);
+}
+
+int msb(int i) {
+    i |= (i >>  1);
+    i |= (i >>  2);
+    i |= (i >>  4);
+    i |= (i >>  8);
+    i |= (i >> 16);
+    return i - ((unsigned)i >> 1);
 }
