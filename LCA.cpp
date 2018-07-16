8bc0c3b9131f3302c565021b41aaf43b6dde30c4
Daniil <steam.haver@gmail.com>
2018-07-16 23:50:34 +0400

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -1,31 +1,3 @@
-/** MIT License Copyright (c) 2018 Vasilyev Daniil **/
-#include <bits/stdc++.h>
-using namespace std;
-#pragma GCC optimize("Ofast")
-template<typename T> using v = vector<T>;
-#define int long long
-typedef string str;
-typedef vector<int> vint;
-#define rep(a, l, r) for(int a = (l); a < (r); a++)
-#define pb push_back
-#define sz(a) ((int) a.size())
-const long long inf = 4611686018427387903; //2^62 - 1
-#if 0  //FileIO
-const string fileName = "";
-ifstream fin ((fileName == "" ? "input.txt"  : fileName + ".in" ));
-ofstream fout((fileName == "" ? "output.txt" : fileName + ".out"));
-#define get fin>>
-#define put fout<<
-#else
-#define get cin>>
-#define put cout<<
-#endif
-#define eol put endl
-void read() {}     template<typename Arg,typename... Args> void read (Arg& arg,Args&... args){get (arg)     ;read(args...) ;}
-void print(){}     template<typename Arg,typename... Args> void print(Arg  arg,Args...  args){put (arg)<<" ";print(args...);}
-void debug(){eol;} template<typename Arg,typename... Args> void debug(Arg  arg,Args...  args){put (arg)<<" ";debug(args...);}
-int getInt(){int a; get a; return a;}
-//code goes here
 const int N = 1e5 + 1;
 
 vector<int> g[N]; //init me
@@ -93,20 +65,3 @@ int distance(int v, int u, int theirLCA = -1) {
 
     return height[v] + height[u] - 2 * height[theirLCA];
 }
-
-void run() {
-    int n;
-    get n;
-    n++;
-    rep(i, 2, n) {
-        int v, u;
-        read(v, u);
-        g[v].pb(u);
-        g[u].pb(v);
-    }
-
-    precalc(1);
-
-}
-
-int32_t main() {srand(time(0)); ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); put fixed; put setprecision(15); run(); return 0;}
