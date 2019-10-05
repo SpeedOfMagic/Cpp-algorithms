de6cb216d7143a56d25df41a5082492599770c82
SpeedOfMagic <steam.haver@gmail.com>
2019-10-05 19:33:15 +0300

    Modified Matrix and Persistent segtree

--- Persistent segment tree.cpp
+++ Persistent segment tree.cpp
@@ -14,12 +14,12 @@ void init() {
 }
 
 int query(int l, int r, int curVersion, int d = 0, int ll = 1, int rr = N) {
-	if (l > r)
-		return 0;
-	if (l == ll && r == rr)
-		return val[curVersion][d];
-	int mid = (ll + rr) >> 1;
-	return query(l, min(r, mid), Left[curVersion][d], d + 1, ll, mid) +
+    if (l > r)
+        return 0;
+    if (l == ll && r == rr)
+        return val[curVersion][d];
+    int mid = (ll + rr) >> 1;
+    return query(l, min(r, mid), Left[curVersion][d], d + 1, ll, mid) +
 		   query(max(l, mid + 1), r, Right[curVersion][d], d + 1, mid + 1, rr);
 }
 
@@ -28,21 +28,21 @@ void update(int num, int v, int prevVersion) {
 	int parallel = prevVersion;
 
 	int cur = versionCount;
-	int ll = 1, rr = N;
-	for (int d = 0; ll != rr; d++) {
-		int mid = (ll + rr) >> 1;
-		if (num > mid) {
-			ll = mid + 1;
+    int ll = 1, rr = N;
+    for (int d = 0; ll != rr; d++) {
+        int mid = (ll + rr) >> 1;
+        if (num > mid) {
+            ll = mid + 1;
 			Left[cur][d] = Left[parallel][d];
 			Right[cur][d] = cur;
 			parallel = Right[parallel][d];
-		} else {
-			rr = mid;
+        } else {
+            rr = mid;
 			Left[cur][d] = cur;
 			Right[cur][d] = Right[parallel][d];
 			parallel = Left[parallel][d];
-		}
-	}
+        }
+    }
 
 	val[cur][D - 1] = v;
 	for (int i = D - 2; i >= 0; i--)
