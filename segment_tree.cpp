5ecba5e22d8ea84c826714ef000a8601958244b9
Daniil Vasilev <steam.haver@gmail.com>
2021-11-09 23:56:45 +0300

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,39 +1,42 @@
 class SegmentTree {
 private:
-    const int nothing_ = -1e9;
-    vector<int> val_;
-    int n_;
+    const int nothing = -1e9;
+    vector<int> val;
+    size_t n;
 
 public:
-    inline int f(int a, int b) { return max(a, b); }
+    static inline int op(int a, int b) { return a + b; }
 
     void update(int pos, int value) {
-        int cur = val.size() + pos - 1;
+        size_t cur = val.size() + pos - 1;
         for (val[cur] = value, cur >>= 1; cur; cur >>= 1)
-            val[cur] = f(val[cur << 1], val[cur << 1 | 1]);
+            val[cur] = op(val[cur << 1], val[cur << 1 | 1]);
     }
 
-    int query(int l, int r, int cur = 1, int ll = 1, int rr = 1e9) {
-        rr = min(rr, n);
+    int query(int l, int r, int cur, int ll, int rr) {
         if (l > r)
             return nothing;
         if (l == ll && r == rr)
             return val[cur];
 
         int mid = (ll + rr) >> 1;
-        return f(query(l, min(r, mid), cur << 1, ll, mid),
-				 query(max(l, mid + 1), r, cur << 1 | 1, mid + 1, rr));
+        return op(query(l, min(r, mid), cur << 1, ll, mid),
+                 query(max(l, mid + 1), r, cur << 1 | 1, mid + 1, rr));
     }
-	
-	SegmentTree(int _n) {
-		for (n = _n; n & (n - 1); n++) {}
-        val = vector<int>(n * 2, nothing);
-	}
 
-    SegmentTree(vector<int> arr) {
+    inline int query(int l, int r) {
+        return query(l, r, 1, 1, static_cast<int>(n));
+    }
+
+    SegmentTree(size_t _n) {
+        for (n = _n; n & (n - 1); n++) {}
+        val = vector<int>(n * 2, nothing);
+    }
+
+    SegmentTree(const vector<int>& arr) {
         for (n = arr.size(); n & (n - 1); n++) {}
         val = vector<int>(n * 2, nothing);
-        for (int i = n + arr.size() - 1; i; i--)
-            val[i] = ((i < n) ? f(val[i << 1], val[i << 1 | 1]) : arr[i - n]);
+        for (size_t i = n + arr.size() - 1; i > 0; --i)
+            val[i] = ((i < n) ? op(val[i << 1], val[i << 1 | 1]) : arr[i - n]);
     }
 };
