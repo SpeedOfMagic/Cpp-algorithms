3956c14ffa606d6318e509dc88679c6c9bbd59c2
Daniil Vasilev <steam.haver@gmail.com>
2021-11-09 23:58:48 +0300

    Update fenwick_tree.cpp

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -1,22 +1,25 @@
 class FenwickTree {
-private:	
+private:
     vector<int> tree_;
 
-public:
-    FenwickTree(int n) : tree_(vector<int>(n, 0)) {}
+    static inline size_t lsb(size_t a) noexcept { return a & -a; }
 
-	inline int lsb(size_t a) const noexcept { return a & -a; }
-    
-    inline int query(size_t r) const noexcept {
+public:
+    explicit FenwickTree(int n) : tree_(vector<int>(n, 0)) {}
+
+    [[nodiscard]] inline int query(size_t r) const noexcept {
         int res = 0;
         for (; r > 0; r -= lsb(r))
             res += tree_[r];
         return res;
     }
 
-    inline void update(size_t position, int change) {
-        for (; position < tree_.size(); position += lsb(pos))
-            tree_[position] += change;
+    [[nodiscard]] inline int query(size_t l, size_t r) const noexcept {
+        return query(r) - query(l - 1);
+    }
+
+    inline void update(size_t pos, int change) {
+        for (; pos < tree_.size(); pos += lsb(pos))
+            tree_[pos] += change;
     }
 };
-
