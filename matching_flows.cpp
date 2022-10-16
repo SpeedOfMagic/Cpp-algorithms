e190c6ea82198c5ee162bd984692ca511a59b4d8
Daniil Vasilev <steam.haver@gmail.com>
2022-10-17 00:19:41 +0300

    Update matching_flows.cpp

--- matching_flows.cpp
+++ matching_flows.cpp
@@ -77,3 +77,74 @@ public:
         return matching;
     }
 };
+
+// Most of the code was used here: https://gist.github.com/Chillee/ad2110fc17af453fb6fc3357a78cfd28
+// I rewrote some things to support initialization and removed non-scaling
+template <class T = int> struct Dinic {
+    int lim = 1;
+    const T INF = numeric_limits<T>::max();
+    struct edge {
+        int to, rev;
+        T cap, flow;
+    };
+
+    int s, t;
+    vector<int> level;
+    vector<uint32_t> ptr;
+    vector<vector<edge>> adj;
+
+    Dinic(int n, int s, int t)
+    : s(s)
+    , t(t)
+    , level(vector<int>(n))
+    , ptr(vector<uint32_t>(n))
+    , adj(vector<vector<edge>>(n))
+    {} 
+
+    void addEdge(int a, int b, T cap, bool isDirected = true) {
+        adj[a].push_back({b, int(adj[b].size()), cap, 0});
+        adj[b].push_back({a, int(adj[a].size()) - 1, isDirected ? 0 : cap, 0});
+    }
+    bool bfs() {
+        queue<int> q({s});
+        level.assign(level.size(), -1);
+        level[s] = 0;
+        while (!q.empty() && level[t] == -1) {
+            int v = q.front();
+            q.pop();
+            for (auto e : adj[v]) {
+                if (level[e.to] == -1 && e.flow < e.cap && e.cap - e.flow >= lim) {
+                    q.push(e.to);
+                    level[e.to] = level[v] + 1;
+                }
+            }
+        }
+        return level[t] != -1;
+    }
+    T dfs(int v, T flow) {
+        if (v == t || !flow)
+            return flow;
+        for (; ptr[v] < adj[v].size(); ptr[v]++) {
+            edge &e = adj[v][ptr[v]];
+            if (level[e.to] != level[v] + 1)
+                continue;
+            if (T pushed = dfs(e.to, min(flow, e.cap - e.flow))) {
+                e.flow += pushed;
+                adj[e.to][e.rev].flow -= pushed;
+                return pushed;
+            }
+        }
+        return 0;
+    }
+    long long calc() {
+        long long flow = 0;
+        for (lim = (1 << 30); lim > 0; lim >>= 1) {
+            while (bfs()) {
+                ptr.assign(ptr.size(), 0);
+                while (T pushed = dfs(s, INF))
+                    flow += pushed;
+            }
+        }
+        return flow;
+    }
+};
